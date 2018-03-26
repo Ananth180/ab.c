40f0b0150984434f07ca87194c04f80c4899a1dd
Ananth180 <35765113+Ananth180@users.noreply.github.com>
2018-03-26 15:56:04 +0530

    Create rs.c

---
+++ rs.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+    int a;
+    scanf("%d",&a);
+    if(a%7==0)
+    printf("Yes");
+    else
+    printf("No");
+    return 0;
+    getch();
+    
+}
